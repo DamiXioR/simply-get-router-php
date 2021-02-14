01837de1b6e45258723c6b2b04927c5aba1e7ae4
damixiorek <damixiorek@gmail.com>
2021-02-14 19:35:27 +0100

    add css styles

--- views/mainView.php
+++ views/mainView.php
@@ -1,2 +1,29 @@
-<?php
-    echo "Inside mainView.php";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Layout</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+
+<body>
+
+    <h1>Simply $_GET Router in PHP</h1>
+    <nav>
+
+        <ul>
+            <li> <a href="">Run action no 1</a> </li>
+            <li> <a href="">Run action no 2</a> </li>
+            <li> <a href="">Run action no 3</a> </li>
+            <li> <a href="">Run action no 4</a> </li>
+        </ul>
+
+    </nav>
+        
+</body>
+
+</html>

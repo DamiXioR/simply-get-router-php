2bfe5359a8f97cdc485b701f492e37fbbbc99a44
damixiorek <damixiorek@gmail.com>
2021-02-15 14:17:01 +0100

    add request class

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@
 
     const ROOT_DIRECTORY = __DIR__;
 
+    $request = new SimplyRouter\Request($_GET, $_POST, $_SERVER);
+
     $controller = new SimplyRouter\Controller();
     $controller->run();
     
---
+++ src/Request.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimplyRouter;
+
+    class Request {
+
+        private array $get;
+        private array $post;
+        private array $server;
+
+        public function __construct(array $get, array $post, array $server){
+            $this->get = $get;
+            $this->post = $post;
+            $this->server = $server;
+        }
+    }
